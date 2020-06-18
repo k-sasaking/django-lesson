165bcf3f797711daa346cc5bb9e3e064b2bb5c75
KazuyaSasaki <kazuya.sasaki39@gmail.com>
2020-06-19 02:37:54 +0900

    Templateの呼び出し: hello.htmlの追加

--- lesson_project/hello/urls.py
+++ lesson_project/hello/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'hello'
 urlpatterns = [
     path('', views.index, name='hello_django'),
+    path('test/template', views.hello_template, name='hello_template'),
 ]
--- lesson_project/hello/views.py
+++ lesson_project/hello/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, Djnago!!")
+
+
+def hello_template(request):
+    template_name = "hello.html"
+    context = {}
+    return render(request, template_name, context)
