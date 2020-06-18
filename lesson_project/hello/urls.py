870e3019d0bcbfe6879dac8c5f63ec78f1f59578
KazuyaSasaki <kazuya.sasaki39@gmail.com>
2020-06-19 03:39:04 +0900

    Templateの基礎とフィルター

--- lesson_project/hello/urls.py
+++ lesson_project/hello/urls.py
@@ -6,4 +6,5 @@ app_name = 'hello'
 urlpatterns = [
     path('', views.index, name='hello_django'),
     path('test/template', views.hello_template, name='hello_template'),
+    path('test/param/', views.hello_param, name='hello_param'),
 ]
--- lesson_project/hello/views.py
+++ lesson_project/hello/views.py
@@ -10,3 +10,26 @@ def hello_template(request):
     template_name = "hello.html"
     context = {}
     return render(request, template_name, context)
+
+
+def hello_param(request):
+    template_name = "hello.html"
+    humans = [
+        TestHuman('Mike', 20, 'us'),
+        TestHuman('Emily', 22, 'us'),
+        TestHuman('Andy', 17, 'us'),
+        TestHuman('Taro', 20, 'jp'),
+        TestHuman('Hanako', 21, 'jp'),
+    ]
+    context = {
+        'test_param': 'これはテストです',
+        'test_humans': humans
+    }
+    return render(request, template_name, context)
+
+
+class TestHuman(object):
+    def __init__(self, name, age, country):
+        self.name = name
+        self.age = age
+        self.country = country
