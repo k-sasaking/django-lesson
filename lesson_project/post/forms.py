204fe6a54209767b7f6485f73c1d0749bfbd5b2b
KazuyaSasaki <kazuya.sasaki39@gmail.com>
2020-06-21 00:33:53 +0900

    Formの基礎

---
+++ lesson_project/post/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post
+
+class PostCreateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('post_name', 'category', 'text', 'is_publish', )
+
+class PostUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
