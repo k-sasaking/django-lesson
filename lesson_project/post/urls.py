0a38a1e01220a969bef9dde0deaac84d1bcda440
KazuyaSasaki <kazuya.sasaki39@gmail.com>
2020-06-21 03:18:55 +0900

    クラス汎用ビューでCRUD

--- lesson_project/post/urls.py
+++ lesson_project/post/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = 'post'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.PostListView.as_view(), name='index'),
+    path('create/', views.PostCreateView.as_view(), name='create'),
+    path('update/<int:pk>', views.PostUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', views.PostDeleteView.as_view(), name='delete'),
 ]
--- lesson_project/post/views.py
+++ lesson_project/post/views.py
@@ -1,8 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+
+from .models import Post
+from .forms import PostCreateForm
 
 
 def index(request):
     template_name = "index.html"
     context = {}
     return render(request, template_name, context)
+
+
+class PostCreateView(CreateView):
+    form_class = PostCreateForm
+    template_name = "post/post_form.html"
+    success_url = reverse_lazy('post:index')
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostCreateForm
+    template_name = "post/post_form.html"
+    success_url = reverse_lazy('post:index')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post/delete_confirm.html'
+    success_url = reverse_lazy('post:index')
